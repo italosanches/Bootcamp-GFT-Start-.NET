77c6eaea305b0a3635501b83bd46a8dad8530483
Italo.sanches <italo_carvalho157@hotmail.com>
2022-02-27 13:17:31 -0300

    Organização de aulas

---
+++ Controle de fluxo/Aula4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+             Boolean condicao = true;
+            int valor;
+            do 
+            {
+                Console.WriteLine("Digite 0 para sair da aplicação");
+                valor = int.Parse(Console.ReadLine());
+                if(valor==0){
+                    Console.WriteLine("Aplicação parada");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"O valor informado foi {valor}");
+                    continue;
+                }
+                
+            }while (condicao ==true);
+        }
+    }
+}
--- Demos/StructPessoa.cs
+++
@@ -1,9 +0,0 @@
-public struct structPessoa
-{
-    public int Idade{get;set;}
-    
-    public string Nome{get;set;}
-
-    public string Documento {get;set;}
-
-}
